46e8ff37d07db7d9c653e31daa448e82195a3224
Atsushi <todaatsushi0@gmail.com>
2019-04-01 18:46:31 +0100

    deployment ready

--- hhts_website/settings.py
+++ hhts_website/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z8l79rn$_1i(k7s*%%1-5xp9ah!zp+_js56cm1@d3v@_#y8192'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
