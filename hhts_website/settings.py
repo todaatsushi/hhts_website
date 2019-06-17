8e01bb4bd50d2568d28a3c3cf60304168e3cbbdc
Atsushi <todaatsushi0@gmail.com>
2019-06-17 16:47:48 +0100

    Updated requriements

--- hhts_website/settings.py
+++ hhts_website/settings.py
@@ -29,7 +29,7 @@ load_dotenv(os.path.join(project_folder, '.env'))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'saijosakaguradouri.pythonanywhere.com']
 
