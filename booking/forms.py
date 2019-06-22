011c3201efd03d14a07d09fbff1cdd52b17aa951
Atsushi <todaatsushi0@gmail.com>
2019-06-22 13:18:11 +0100

    Add translation for booking models and forms

--- booking/forms.py
+++ booking/forms.py
@@ -17,68 +17,68 @@ class BookTourForm(forms.ModelForm):
         self.helper.layout = cf.Layout(
 
             cf.Fieldset(
-                '基本情報',
+                _('基本情報'),
                 cf.Div(
                     cf.Div(
-                        cf.Field('scheduled_at', css_class='mr-3', placeholder='予定時間'),
-                        cf.Field('duration', css_class='mr-3', placeholder='期間'),
+                        cf.Field('scheduled_at', css_class='mr-3', placeholder=_('予定時間')),
+                        cf.Field('duration', css_class='mr-3', placeholder=_('期間')),
                         cf.HTML(
-                            """
+                            f"""
                             <select name="transportation" class="mr-3 form-control" required="" id="id_transportation">
-                              <option value="使う交通手段" selected disabled>使う交通手段</option>
-                              <option value="電車">電車</option>
-                              <option value="コーチ">コーチ</option>
-                              <option value="自動車・バン">自動車・バン</option>
-                              <option value="タクシ">タクシ</option>
-                              <option value="その他">他の交通手段（”他に伝えたい情報”で伝えてください。）</option>
+                              <option value="{_("使う交通手段")}" selected disabled>{_("使う交通手段")}</option>
+                              <option value="{_("電車")}">{_("電車")}</option>
+                              <option value="{_("コーチ")}">{_("コーチ")}</option>
+                              <option value="{_("自動車・バン")}">{_("自動車・バン")}</option>
+                              <option value="{_("タクシ")}">{_("タクシ")}</option>
+                              <option value="{_("その他")}">{_("他の交通手段は後で伝えてください")}</option>
                             </select>
                             """
                         ),
                         css_class='col-md-6'
                     ),
                     cf.Div(
-                        cf.Field('places_to_visit', placeholder='訪問したい酒蔵', rows='5'),
+                        cf.Field('places_to_visit', placeholder=_('訪問したい酒蔵'), rows='5'),
                         css_class='col-md-6'
                     ),
                     css_class='row form-group'
                 )
             ),
             cf.Fieldset(
-                '個人情報',
+                _('個人情報'),
                 cf.Div(
                     cf.Div(
-                        cf.Field('contact_name', css_class='mr-3', placeholder='名前'),
-                        cf.Field('contact_number', css_class='mr-3', placeholder='電話番号'),
-                        cf.Field('contact_email', css_class='mr-3', placeholder='メールアドレス'),
+                        cf.Field('contact_name', css_class='mr-3', placeholder=_('名前')),
+                        cf.Field('contact_number', css_class='mr-3', placeholder=_('電話番号')),
+                        cf.Field('contact_email', css_class='mr-3', placeholder=_('メールアドレス')),
                         css_class='col-md-6'
                     ),
                     cf.Div(
-                        cf.Field('contact_address', placeholder='住所', rows='5'),
+                        cf.Field('contact_address', placeholder=_('住所'), rows='5'),
                         css_class='col-md-6'
                     ),
                     css_class='row form-group'
                 )
             ),
             cf.Fieldset(
-                'グループの情報',
+                _('グループの情報'),
                 cf.Div(
                     cf.HTML(
-                        """
-                        <input type="checkbox" name="is_group" class="mr-3 checkboxinput" placeholder="グループ予約" id="id_is_group" checked="">
-                        <label for="is_group" class='mr-3'>グループ予約</label>
+                        f"""
+                        <input type="checkbox" name="is_group" class="mr-3 checkboxinput" placeholder="{_("グループ予約")}" id="id_is_group" checked="">
+                        <label for="is_group" class='mr-3'>{_('グループ予約')}</label>
                         """
                     ),
-                    cf.Field('group_name', placeholder='グループ名前', css_class='mr-3'),
-                    cf.Field('group_number', placeholder='グループの数', css_class='mr-3'),
+                    cf.Field('group_name', placeholder=_('グループ名前'), css_class='mr-3'),
+                    cf.Field('group_number', placeholder=_('グループの数'), css_class='mr-3'),
                     cf.HTML(
-                        """
+                        f"""
                         <select name="age_group" class="mr-3 select form-control" required="" id="id_age_group">
-                            <option value="NA" selected disabled>年齢層</option>
-                            <option value="家族">家族</option>
-                            <option value="大人">大人</option>
-                            <option value="高齢者">高齢者</option>
-                            <option value="子供・思春期">子供・思春期</option>
-                            <option value="ミックス">ミックス</option>
+                            <option value="NA" selected disabled>{_('年齢層')}</option>
+                            <option value={_('家族')}>{_('家族')}</option>
+                            <option value={_('大人')}>{_('大人')}</option>
+                            <option value={_('高齢者')}>{_('高齢者')}</option>
+                            <option value={_('子供・思春期')}>{_('子供・思春期')}</option>
+                            <option value={_('ミックス')}>{_('ミックス')}</option>
                         </select>
                         """
                     ),
@@ -86,20 +86,18 @@ class BookTourForm(forms.ModelForm):
                 )
             ),
             cf.Fieldset(
-                '確認',
+                _('そのほかと確認'),
                 cf.Div(
                     cf.Div(
-                        cf.Field('extra_details', placeholder='他に伝えたい情報', rows='5'),
+                        cf.Field('extra_details', placeholder=_('他に伝えたい情報'), rows='5'),
                         css_class='col-md-6'
                     ),
                     cf.Div(
                         cf.HTML(
-                            """
-                            <a href="{% url 'home' %}" class='btn btn-sm btn-block btn-custom-1 my-3'>戻る</a>
-                            """
+                            '<a href="{% url ' + '"home"' + ' %}" class="btn btn-sm btn-block btn-custom-1 my-3">' + f"{_('戻る')}</a>"
                         ),
                         cf.ButtonHolder(
-                            cf.Submit('submit', '送る', css_class='btn btn-sm btn-block btn-success my-3')
+                            cf.Submit('submit', _('送る'), css_class='btn btn-sm btn-block btn-success my-3')
                         ),
                         css_class='col-md-4 offset-1'
                     ),
--- booking/models.py
+++ booking/models.py
@@ -9,38 +9,38 @@ from .email_helper import ConfirmEmail
 class Booking(models.Model):
 
     AGE_GROUPS = (
-        ('家族', '家族'),
-        ('大人', '大人'),
-        ('高齢者', '高齢者'),
-        ('子供・思春期', '子供・思春期'),
-        ('ミックス', 'ミックス'),
+        (_('家族'), _('家族')),
+        (_('大人'), _('大人')),
+        (_('高齢者'), _('高齢者')),
+        (_('子供・思春期'), _('子供・思春期')),
+        (_('ミックス'), _('ミックス')),
     )
 
     TRANSPORT_OPTIONS = (
-        ('使う交通手段', '使う交通手段'),
-        ('電車', '電車'),
-        ('コーチ', 'コーチ'),
-        ('自動車・バン', '自動車・バン'),
-        ('タクシ', 'タクシ'),
-        ('その他', '他の交通手段（”他に伝えたい情報”で伝えてください。）'),
+        (_('使う交通手段'), _('使う交通手段')),
+        (_('電車'), _('電車')),
+        (_('コーチ'), _('コーチ')),
+        (_('自動車・バン'), _('自動車・バン')),
+        (_('タクシ'), _('タクシ')),
+        (_('その他'), _('他の交通手段は後で伝えてください')),
     )
 
-    contact_name = models.CharField(max_length=50, verbose_name='名前')
-    is_group = models.BooleanField(default=True, verbose_name='グループ予約')
-    group_name = models.CharField(max_length=100, null=True, verbose_name='グループ名前')
-    group_number = models.IntegerField(null=True, verbose_name='グループの数')
-    contact_number = models.CharField(max_length=50, verbose_name='電話番号')
-    contact_address = models.TextField(verbose_name='住所')
-    contact_email = models.EmailField(verbose_name='メールアドレス')
-    scheduled_at = models.DateTimeField(verbose_name='予定時間')
-    duration = models.CharField(max_length=50, verbose_name='期間（分）')
-    age_group = models.CharField(max_length=50, choices=AGE_GROUPS, verbose_name='年齢層')
-    places_to_visit = models.TextField(verbose_name='訪問したい酒蔵')
+    contact_name = models.CharField(max_length=50, verbose_name=_('名前'))
+    is_group = models.BooleanField(default=True, verbose_name=_('グループ予約'))
+    group_name = models.CharField(max_length=100, null=True, verbose_name=_('グループ名前'))
+    group_number = models.IntegerField(null=True, verbose_name=_('グループの数'))
+    contact_number = models.CharField(max_length=50, verbose_name=_('電話番号'))
+    contact_address = models.TextField(verbose_name=_('住所'))
+    contact_email = models.EmailField(verbose_name=_('メールアドレス'))
+    scheduled_at = models.DateTimeField(verbose_name=_('予定時間'))
+    duration = models.CharField(max_length=50, verbose_name=_('期間・分'))
+    age_group = models.CharField(max_length=50, choices=AGE_GROUPS, verbose_name=_('年齢層'))
+    places_to_visit = models.TextField(verbose_name=_('訪問したい酒蔵'))
     transportation = models.CharField(max_length=50, choices=TRANSPORT_OPTIONS, verbose_name='使う交通手段')
-    extra_details = models.TextField(verbose_name='他に伝えたい情報')
-    booked_at = models.DateTimeField(default=timezone.now, verbose_name='予約をとった時間')
-    confirmed = models.BooleanField(default=False, verbose_name='確認されたが')
-    complete = models.BooleanField(default=False, verbose_name='完成した')
+    extra_details = models.TextField(verbose_name=_('他に伝えたい情報'))
+    booked_at = models.DateTimeField(default=timezone.now, verbose_name=_('予約をとった時間'))
+    confirmed = models.BooleanField(default=False, verbose_name=_('確認されたが'))
+    complete = models.BooleanField(default=False, verbose_name=_('完成した'))
 
     class Meta:
         ordering = ['-scheduled_at', '-booked_at', 'confirmed']
