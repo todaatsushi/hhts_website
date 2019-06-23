70980e8d99eaa4ca7e3ba94a86a027fae076ffda
Atsushi <todaatsushi0@gmail.com>
2019-06-23 20:26:24 +0100

    Refactor view tests to use helper function, also add extra tests for the detailview refactor

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 from blog.models import Post
 import blog.tests.helper as h
+from booking.tests.helper import basic_view_check
 
 
 class BlogViewsTestCase(TestCase):
@@ -19,108 +20,85 @@ class BlogViewsTestCase(TestCase):
         return self.settings(LANGUAGE_CODE='en')
 
     def test_public_post_view(self):
-        response = self.client.get(reverse('blog-home'))
-
-        # Successful check
-        self.assertEqual(response.status_code, 200)
-
-        # Template check
-        self.assertTemplateUsed(response, 'blog/public_posts.html')
-
-        # Right text (Japanese)
-        self.assertContains(response, 'ブロッグ')
-
-        # No admin posts
-        self.assertNotContains(response, 'Admin test post')
+        basic_view_check(
+            test_case_obj=self,
+            path=reverse('blog-home'),
+            template='blog/public_posts.html',
+            login_required=False,
+            login_desired=True,
+            text_on_page=[
+                'ブロッグ',
+                '*!Admin test post'
+            ]
+        )
 
     def test_public_post_view_en(self):
         with self.english_setting():
-            response = self.client.get(reverse('blog-home'))
-
-            # Successful check
-            self.assertEqual(response.status_code, 200)
-
-            # Template check
-            self.assertTemplateUsed(response, 'blog/public_posts.html')
-
-            # Right text (Japanese)
-            self.assertContains(response, 'Blog')
-            
-            # No admin posts
-            self.assertNotContains(response, 'Admin test post')
+            basic_view_check(
+                self,
+                reverse('blog-home'),
+                'blog/public_posts.html',
+                False,
+                True,
+                [
+                    'Blog',
+                    '*!Admin test post'
+                ]
+            )
 
     def test_blog_user_post_view_logged_out(self):
-        # Make sure admin post is hidden
-        response = self.client.get(reverse('post-user', kwargs={'username': self.user.username}))
-
-        # Successful check
-        self.assertEqual(response.status_code, 200)
-
-        # Template check
-        self.assertTemplateUsed(response, 'blog/user_posts.html')
-
-        # Right text (Japanese)
-        self.assertContains(response, f'{self.user.profile.name} のポスト')
-
-        # Ensure admin post is not shown
-        self.assertNotContains(response, 'Admin test post')
+        basic_view_check(
+            self,
+            reverse('post-user', kwargs={'username': self.user.username}),
+            'blog/user_posts.html',
+            False,
+            False,
+            [
+                f'{self.user.profile.name} のポスト',
+                '*!Admin test post'
+            ]
+        )
 
     def test_blog_user_post_view_logged_out_en(self):
         with self.english_setting():
-            # Make sure admin post is hidden
-
-            response = self.client.get(reverse('post-user', kwargs={'username': self.user.username}))
-
-            # Successful check
-            self.assertEqual(response.status_code, 200)
-
-            # Template check
-            self.assertTemplateUsed(response, 'blog/user_posts.html')
-
-            # Right text (English)
-            self.assertContains(response, f'{self.user.profile.name} All Posts')
-
-            # Ensure admin post is not shown
-            self.assertNotContains(response, 'Admin test post')
+            basic_view_check(
+                self,
+                reverse('post-user', kwargs={'username': self.user.username}),
+                'blog/user_posts.html',
+                False,
+                False,
+                [
+                    f'{self.user.profile.name} All Posts',
+                    '*!Admin test post'
+                ]
+            )
 
     def test_blog_user_post_view_logged_in(self):
-        # Login
-        self.client.force_login(User.objects.first())
-
-        # Make sure admin post is hidden
-        response = self.client.get(reverse('post-user', kwargs={'username': self.user.username}))
-
-        # Successful check
-        self.assertEqual(response.status_code, 200)
-
-        # Template check
-        self.assertTemplateUsed(response, 'blog/user_posts.html')
-
-        # Right text (Japanese)
-        self.assertContains(response, f'{self.user.profile.name} のポスト')
-
-        # Ensure admin post is not shown
-        self.assertContains(response, 'Admin test post')
+        basic_view_check(
+            self,
+            reverse('post-user', kwargs={'username': self.user.username}),
+            'blog/user_posts.html',
+            False,
+            True,
+            [
+                f'{self.user.profile.name} のポスト',
+                'Admin test post'
+            ]
+        )
 
     def test_blog_user_post_view_logged_in_en(self):
         with self.english_setting():
-            self.client.force_login(User.objects.first())
-            # Make sure admin post is hidden
-            # Make admin post
-
-            response = self.client.get(reverse('post-user', kwargs={'username': self.user.username}))
-
-            # Successful check
-            self.assertEqual(response.status_code, 200)
-
-            # Template check
-            self.assertTemplateUsed(response, 'blog/user_posts.html')
-
-            # Right text (English)
-            self.assertContains(response, f'{self.user.profile.name} All Posts')
-
-            # Ensure admin post is shown
-            self.assertContains(response, 'Admin test post')
+            basic_view_check(
+                self,
+                reverse('post-user', kwargs={'username': self.user.username}),
+                'blog/user_posts.html',
+                False,
+                True,
+                [
+                    f'{self.user.profile.name} All Posts',
+                    'Admin test post'
+                ]
+            )
 
     def test_admin_post_view_forbidden(self):
         # No login
@@ -129,123 +107,232 @@ class BlogViewsTestCase(TestCase):
         # Should redirect to login view
         self.assertEqual(response.status_code, 302)
 
-    def test_admin_post_view_logged_in(self):
-        self.client.force_login(User.objects.first())
-        response = self.client.get(reverse('post-admin'))
+    def test_admin_posts_view_logged_in(self):
+        basic_view_check(
+            self,
+            reverse('post-admin'),
+            'blog/admin_posts.html',
+            True,
+            True,
+            [
+                'スタフ専用',
+                'Admin test post'
+            ]
+        )
 
-        self.assertEqual(response.status_code, 200)
-
-        self.assertTemplateUsed(response, 'blog/admin_posts.html')
-
-        # Right text (English)
-        self.assertContains(response, 'スタフ専用')
-
-        # Ensure admin post is shown
-        self.assertContains(response, 'Admin test post')
-
-    def test_admin_post_view_logged_in_en(self):
-        self.client.force_login(User.objects.first())
-        
+    def test_admin_posts_view_logged_in_en(self):
         with self.english_setting():
-            response = self.client.get(reverse('post-admin'))
-
-            self.assertEqual(response.status_code, 200)
-
-            self.assertTemplateUsed(response, 'blog/admin_posts.html')
-
-            # Right text (English)
-            self.assertContains(response, 'Staff only posts')
-
-            # Ensure admin post is shown
-            self.assertContains(response, 'Admin test post')
-
-    def test_post_detail_view(self):
-        response = self.client.get(reverse('post-detail', kwargs={'pk': 1}))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'blog/post.html')
-        self.assertContains(response, 'Test content')
-        
-        # Test button text for japanese
-        self.assertContains(response, '更新')
-
-    def test_post_detail_view_en(self):
-        with self.english_setting():
-            response = self.client.get(reverse('post-detail', kwargs={'pk': 1}))
-            self.assertEqual(response.status_code, 200)
-            self.assertTemplateUsed(response, 'blog/post.html')
-            self.assertContains(response, 'Test content')
-            
-            # Test button text for english
-            self.assertContains(response, 'Update')
+            basic_view_check(
+                self,
+                reverse('post-admin'),
+                'blog/admin_posts.html',
+                True,
+                True,
+                [
+                    'Staff only posts',
+                    'Admin test post'
+                ]
+            )
 
     def test_create_view(self):
-        self.client.force_login(User.objects.first())
-        response = self.client.get(reverse('post-new'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'blog/create.html')
-        self.assertContains(response, 'ポストを新しく作る')
+        basic_view_check(
+            self,
+            reverse('post-new'),
+            'blog/create.html',
+            True,
+            True,
+            [
+                'ポストを新しく作る',
+            ]
+        )
 
     def test_create_view_en(self):
-        self.client.force_login(User.objects.first())
         with self.english_setting():
-            response = self.client.get(reverse('post-new'))
-            self.assertEqual(response.status_code, 200)
-            self.assertTemplateUsed(response, 'blog/create.html')
-            self.assertContains(response, 'Create')
+            basic_view_check(
+                self,
+                reverse('post-new'),
+                'blog/create.html',
+                True,
+                True,
+                [
+                    'Create',
+                ]
+            )
 
     def test_update_view(self):
-        self.client.force_login(self.user)
-        response = self.client.get(reverse('post-update', kwargs={'pk': 1}))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'blog/update.html')
-        self.assertContains(response, '更新')
+        basic_view_check(
+            self,
+            reverse('post-update', kwargs={'pk': self.post.pk}),
+            'blog/update.html',
+            True,
+            True,
+            [   
+                'ポストの更新',
+            ]
+        )
 
     def test_update_view_en(self):
-        self.client.force_login(self.user)
-        response = self.client.get(reverse('post-update', kwargs={'pk': 1}))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'blog/update.html')
-        self.assertContains(response, 'ポストの更新')
+        with self.english_setting():
+            basic_view_check(
+                self,
+                reverse('post-update', kwargs={'pk': self.post.pk}),
+                'blog/update.html',
+                True,
+                True,
+                [   
+                    'Update post',
+                ]
+            )
 
     def test_delete_view(self):
-        self.client.force_login(self.user)
-        response = self.client.get(reverse('post-delete', kwargs={'pk': 1}))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'blog/confirm_delete.html')
-        self.assertContains(response, 'ポスト削除')
+        basic_view_check(
+            self,
+            reverse('post-delete', kwargs={'pk': self.post.pk}),
+            'blog/confirm_delete.html',
+            True,
+            True,
+            [   
+                'ポスト削除',
+            ]
+        )
 
     def test_delete_view_en(self):
         with self.english_setting():
-            self.client.force_login(self.user)
-            response = self.client.get(reverse('post-delete', kwargs={'pk': 1}))
-            self.assertEqual(response.status_code, 200)
-            self.assertTemplateUsed(response, 'blog/confirm_delete.html')
-            self.assertContains(response, 'Delete post')
+            basic_view_check(
+                self,
+                reverse('post-delete', kwargs={'pk': self.post.pk}),
+                'blog/confirm_delete.html',
+                True,
+                True,
+                [   
+                    'Delete post',
+                ]
+            )
 
     def test_post_comment(self):
-        response = self.client.get(reverse('post-comment', kwargs={'pk': 1}))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'blog/comment.html')
-        self.assertContains(response, 'コメントを残す')
+        basic_view_check(
+            self,
+            reverse('post-comment', kwargs={'pk': self.post.pk}),
+            'blog/comment.html',
+            False,
+            True,
+            [   
+                'コメントを残す',
+            ]
+        )
 
     def test_post_comment_en(self):
         with self.english_setting():
-            response = self.client.get(reverse('post-comment', kwargs={'pk': 1}))
-            self.assertEqual(response.status_code, 200)
-            self.assertTemplateUsed(response, 'blog/comment.html')
-            self.assertContains(response, 'Comment')
+            basic_view_check(
+                self,
+                reverse('post-comment', kwargs={'pk': self.post.pk}),
+                'blog/comment.html',
+                False,
+                True,
+                [   
+                    'Comment',
+                ]
+            )
 
     def test_post_comment_delete_view(self):
-        self.client.force_login(self.user)
-        response = self.client.get(reverse('post-comment_delete', kwargs={'pk': self.post.pk, 'pkc': self.comment.pk}))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'blog/delete_comment.html')
-        self.assertContains(response, 'コメント')
+        basic_view_check(
+            self,
+            reverse('post-comment_delete', kwargs={'pk': self.post.pk, 'pkc': self.comment.pk}),
+            'blog/delete_comment.html',
+            True,
+            True,
+            [   
+                'コメント',
+            ]
+        )
 
     def test_post_comment_delete_view(self):
         with self.english_setting():
-            self.client.force_login(self.user)
-            response = self.client.get(reverse('post-comment_delete', kwargs={'pk': self.post.pk, 'pkc': self.comment.pk}))
-            self.assertEqual(response.status_code, 200)
-            self.assertTemplateUsed(response, 'blog/delete_comment.html')
-            self.assertContains(response, 'Delete')
+            basic_view_check(
+                self,
+                reverse('post-comment_delete', kwargs={'pk': self.post.pk, 'pkc': self.comment.pk}),
+                'blog/delete_comment.html',
+                True,
+                True,
+                [   
+                    'Delete',
+                ]
+            )
+        
+    def test_post_detail_view_logged_out_and_viewing_normal_post(self):
+        basic_view_check(
+            self,
+            reverse('post-detail', kwargs={'pk': self.post.pk}),
+            'blog/post.html',
+            False,
+            False,
+            [
+                'Test content',
+                '更新', # Button text on the page
+            ]
+        )
+
+    def test_post_detail_view_logged_out_and_viewing_normal_post_en(self):
+        with self.english_setting():
+            basic_view_check(
+                self,
+                reverse('post-detail', kwargs={'pk': self.post.pk}),
+                'blog/post.html',
+                False,
+                False,
+                [
+                    'Test content',
+                    'Update', # Button text on the page
+                ]
+            )
+
+    def test_post_detail_view_logged_in_and_viewing_admin_post(self):
+        basic_view_check(
+            self,
+            reverse('post-detail', kwargs={'pk': self.admin_post.pk}),
+            'blog/post.html',
+            True,
+            True,
+            [
+                'Admin test post'
+            ]
+        )
+
+    def test_post_detail_view_logged_out_and_viewing_admin_post(self):
+        basic_view_check(
+            self,
+            reverse('post-detail', kwargs={'pk': self.admin_post.pk}),
+            'None', # Should end in redirect so no template needed
+            True,
+            False,
+            [] # Same as template
+        )
+
+    def test_post_detail_view_logged_in_and_viewing_normal_post(self):
+        basic_view_check(
+            self,
+            reverse('post-detail', kwargs={'pk': self.post.pk}),
+            'blog/post.html',
+            False,
+            True,
+            [
+                'Test content',
+                '更新'
+            ]
+        )
+
+    def test_post_detail_view_logged_in_and_viewing_normal_post_en(self):
+        with self.english_setting():
+            basic_view_check(
+                self,
+                reverse('post-detail', kwargs={'pk': self.post.pk}),
+                'blog/post.html',
+                False,
+                True,
+                [
+                    'Test content',
+                    'Update'
+                ]
+            )
+
+
