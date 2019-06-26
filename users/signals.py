faa9fce928f85ccc0057a32b5f6f7650850bd5e3
Atsushi <todaatsushi0@gmail.com>
2019-06-26 17:37:18 +0100

    Fix signals bug, finish model tests

--- users/signals.py
+++ users/signals.py
@@ -1,13 +1,15 @@
-"""
-Automatically create profile when User instance is made.
-"""
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.core.files.storage import default_storage
+from django.conf import settings
 
-from .models import Profile
+import os
+
+from users.models import Profile
 
 
+# Automatically create profile when User instance is made.
 # If post_save is made (user is made, create profile)
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
@@ -19,3 +21,25 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+# Delete unused profile pictures on delete and on update
+@receiver(post_delete, sender=User)
+@receiver(post_save, sender=Profile)
+@receiver(post_delete, sender=Profile)
+def delete_unused_image_files(sender, instance, **kwargs):
+    # All images currently in use
+    in_use = [
+            profile.image.url.replace('/media/', f'{settings.MEDIA_ROOT}/') for profile in Profile.objects.all()
+    ]
+
+    # Image locations
+    image_dir = os.path.join(settings.MEDIA_ROOT, 'profile_pics')
+    all_pics = [
+        f'{image_dir}/{pic}' for pic in os.listdir(image_dir)
+    ]
+
+    not_needed = [path for path in all_pics if path not in in_use]
+    for path in not_needed:
+        if path:
+            default_storage.delete(path)
--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -19,6 +19,10 @@ class ProfileModelTestCase(TestCase):
         with self.testing_media():
             self.user = h.create_user()
             self.profile = self.user.profile # Profile is created by signal
+            self.data = self.profile.__dict__
+            self.data['name'] = 'Brian'
+            self.data['about'] = 'Best driver in the Fast crew.'
+            self.data['role'] = 'Driver'
 
     def create_image(self, name='test.png'):
         return h.create_dummy_image(name)
@@ -32,18 +36,12 @@ class ProfileModelTestCase(TestCase):
             f'{self.profile.name} - {self.user.username}'
         )
 
-    def test_old_images_are_deleted_on_instance_delele(self):
+    def test_old_images_are_deleted_on_profile_instance_delete(self):
         with self.testing_media():
             # https://stackoverflow.com/a/34276961
             img = self.create_image()
-            data = {
-                'image': img,
-                'name': "Brian O'Conner",
-                'about': 'One of the leaders of the team and a world class driver.',
-                'role': 'Driver',
-            }
-
-            form = ProfileUpdateForm(instance=self.profile, data=data)
+            self.data['image'] = img
+            form = ProfileUpdateForm(instance=self.profile, data=self.data)
             self.assertTrue(form.is_valid())
 
             form.save()
@@ -59,3 +57,52 @@ class ProfileModelTestCase(TestCase):
             if image_path.is_file():
                 image_path.unlink()
                 raise  AssertionError('File has not been deleted.')
+
+    def test_old_images_are_deleted_on_user_instance_delete(self):
+        with self.testing_media():
+            # https://stackoverflow.com/a/34276961
+            img = self.create_image()
+            self.data['image'] = img
+
+            form = ProfileUpdateForm(instance=self.profile, data=self.data)
+            self.assertTrue(form.is_valid())
+
+            form.save()
+
+            image_path = pathlib.Path(
+                os.path.join(settings.MEDIA_ROOT, 'profile_pics/test.png')
+            )
+
+            self.assertTrue(image_path.is_file())
+
+            User.objects.first().delete()
+
+            if image_path.is_file():
+                image_path.unlink()
+                raise  AssertionError('File has not been deleted.')
+
+    def test_old_images_are_deleted_on_profile_update(self):
+        with self.testing_media():
+            # https://stackoverflow.com/a/34276961
+            img = self.create_image()
+            img2 = self.create_image('testing2.png')
+            self.data['image'] = img
+
+            form = ProfileUpdateForm(instance=self.profile, data=self.data)
+            self.assertTrue(form.is_valid())
+
+            form.save()
+
+            image_path = pathlib.Path(
+                os.path.join(settings.MEDIA_ROOT, 'profile_pics/test.png')
+            )
+
+            self.assertTrue(image_path.is_file())
+
+            # Update data and user profile with new image
+            self.data['image'] = img2
+            form = ProfileUpdateForm(instance=self.profile, data=self.data)
+            self.assertTrue(form.is_valid())
+            form.save()
+
+            self.assertFalse(image_path.is_file()) # old image should be gone
